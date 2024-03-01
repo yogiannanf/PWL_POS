8aaa04c223ae06ee53eec448551d08f55abbbe65
yogiannanf <yogiannanf@gmail.com>
2024-03-01 10:09:16 +0700

    Modifikasi Praktikum 2

--- database/migrations/2024_03_01_025720_create_m_kategori_table.php
+++ database/migrations/2024_03_01_025720_create_m_kategori_table.php
@@ -11,8 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
+
         Schema::create('m_kategori', function (Blueprint $table) {
-            $table->id();
+            $table->id('kategori_id');
+            $table->string('kategori_kode', 10)->unique();
+            $table->string('kategori_nama', 100);
             $table->timestamps();
         });
     }
