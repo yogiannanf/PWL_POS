466327e613bc12044a13f1cb4623567038c9618a
yogiannanf <yogiannanf@gmail.com>
2024-03-15 08:45:06 +0700

    Modifikasi Jobsheet 3

---
+++ database/seeders/BarangSeeder.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class BarangSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $data = [
+            [
+                'kategori_id' => 1,
+                'kategori_kode' => 'YN001',
+                'kategori_nama' => 'Furniture ruang tamu',
+            ],
+            [
+                'kategori_id' => 2,
+                'kategori_kode' => 'YN002',
+                'kategori_nama' => 'Furniture kamar tidur',
+            ],
+            [
+                'kategori_id' => 3,
+                'kategori_kode' => 'YN003',
+                'kategori_nama' => 'Furniture ruang makan',
+            ],
+            [
+                'kategori_id' => 4,
+                'kategori_kode' => 'YN004',
+                'kategori_nama' => 'Furniture dapur',
+            ],
+            [
+                'kategori_id' => 5,
+                'kategori_kode' => 'YN005',
+                'kategori_nama' => 'Furniture ruang kerja',
+            ],
+        ];
+        DB::table('m_kategori')->insert($data);
+    }
+}
