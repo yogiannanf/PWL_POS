7c0eba7102439dd88108d6844d321f48ac4ba83b
yogiannanf <yogiannanf@gmail.com>
2024-03-17 01:13:07 +0700

    Modifikasi Jobsheet 3

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -13,12 +13,14 @@ class UserController extends Controller
     {
     // tambah data user dengan Eloquent Model
     $data = [
-        'username' => 'customer-1',
-        'nama' => 'Pelanggan',
-        'password' => Hash::make('12345'),
-        'level_id' => 4
+        'nama' => 'Pelanggan Pertama'
+        // 'username' => 'customer-1',
+        // 'nama' => 'Pelanggan',
+        // 'password' => Hash::make('12345'),
+        // 'level_id' => 4
     ];
-    UserModel::insert($data); //tambahkan data ke tabel m_user
+    UserModel::where('username', 'customer-1')->update($data); //update data user
+    //UserModel::insert($data); //tambahkan data ke tabel m_user
 
     //coba akses model UserModel
     $user = UserModel::all();//ambil semua data dari tabel m_user
