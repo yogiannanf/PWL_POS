017c078b99cdf96fb45d374ecf05353c14cb64e1
yogiannanf <yogiannanf@gmail.com>
2024-03-21 21:11:19 +0700

    Modifikasi Jobsheet 4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,11 +11,22 @@ class UserController extends Controller
 {
     public function index()
     {
-        $user = UserModel::findor(20, ['username', 'nama'], function(){
-            abort(404);
-        });
-
+        $user = UserModel::where('level_id', 2)->count();
+        //dd($user);
+        //$user = UserModel::where('username', 'manager9')->firstOrFail();
         return view('user', ['data' => $user]);
+
+        /* $user = UserModel::findOrFail(1);
+        return view('user', ['data' => $user]);
+        */
+
+        /** 2.1 nomor 10
+        *$user = UserModel::findor(20, ['username', 'nama'], function(){
+        *    abort(404);
+        *});
+
+        *return view('user', ['data' => $user]);
+        */
 
         /** 2.1 nomor 4
         * $user = UserModel::where('level_id', 1)->first();
