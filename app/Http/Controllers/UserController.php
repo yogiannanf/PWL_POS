a55358da9d65c647c41f33802f27f3b7426c6de6
yogiannanf <yogiannanf@gmail.com>
2024-04-25 17:54:13 +0700

    Praktikum 4 – Implementasi Filtering Datatables: JS 7

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -21,7 +21,9 @@ class UserController extends Controller
         ];
         $activeMenu = 'user'; //set menu yang sedang aktif
 
-        return view('user.index', ['breadcrumb' => $breadcrumb, 'page' => $page,  'activeMenu' => $activeMenu]);
+        $level = LevelModel::all(); //ambil data level untuk filter level
+
+        return view('user.index', ['breadcrumb' => $breadcrumb, 'page' => $page, 'level' => $level, 'activeMenu' => $activeMenu]);
     }
     // Ambil data user dalam bentuk json untuk datatables 
     public function list(Request $request)
@@ -29,6 +31,10 @@ class UserController extends Controller
         $users = UserModel::select('user_id', 'username', 'nama', 'level_id')
             ->with('level');
 
+        //Filter data user berdasarkan level_id
+        if ($request->level_id) {
+            $users->where('level_id', $request->level_id);
+        }
         return DataTables::of($users)
             ->addIndexColumn() // menambahkan kolom index / no urut (default nama kolom: DT_RowIndex)
             ->addColumn('aksi', function ($user) { // menambahkan kolom aksi
--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -16,6 +16,22 @@
     @if (@session('error'))
         <div class="alert alert-danger">{{ session('error')}}</div>
     @endif
+    <div class="row">
+        <div class="col-md-12">
+            <div class="form-group row">
+                <label class="col-1 control-label col-form-label">Filter:</label>
+                <div class="col-3">
+                    <select class="form-control" id="level_id" name="level_id" required>
+                        <option value="">- Semua -</option>
+                        @foreach ($level as $item)
+                            <option value="{{ $item->level_id }}">{{ $item->level_nama }}</option>
+                        @endforeach
+                    </select>
+                    <small class="form-text text-muted">Level Pengguna</small>
+                </div>
+            </div>
+        </div>
+    </div>
     <table class="table table-bordered table-striped table-hover table-sm" id="table_user">
         <thead>
             <tr><th>ID</th><th>Username</th><th>Nama</th><th>Level Pengguna</th><th>Aksi</th></tr>
@@ -34,7 +50,9 @@ ajax: {
     "url": "{{ url('user/list') }}", 
     "dataType": "json",
     "type": "POST",
-    
+    "data": function (d){
+        d.level_id = $('#level_id').val();
+    }
 },
 columns: [
 {
@@ -61,7 +79,9 @@ searchable: false	// searchable: true, jika ingin kolom ini bisa dicari
 }
 ]
 });
-
+$('#level_id').on('change', function(){
+    dataUser.ajax.reload();
+});
 });
 </script> 
 @endpush
