e935f3554b0de20445c0abe32dacf58526e95812
yogiannanf <yogiannanf@gmail.com>
2024-06-01 19:02:44 +0700

    JS 11_Tugas tabel m_barang

--- app/Models/BarangModel.php
+++ app/Models/BarangModel.php
@@ -5,17 +5,35 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
 class BarangModel extends Model
 {
     use HasFactory;
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+    
     protected $table = 'm_barang';
     protected $primaryKey = 'barang_id';
 
-    protected $fillable = ['barang_id', 'kategori_id', 'barang_kode', 'barang_nama', 'harga_beli', 'harga_jual'];
-
+    protected $fillable = ['barang_id', 'kategori_id', 'barang_kode', 'barang_nama', 'harga_beli', 'harga_jual', 'image'];
     public function kategori(): BelongsTo
     {
         return $this->belongsTo(KategoriModel::class, 'kategori_id', 'kategori_id');
+    }
+
+    protected function image(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($image) => url('/storage/posts/' . $image),
+        );
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -21,6 +21,25 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+
+
+
+// CRUD transaksi
+//Route::get('transaksi', [PenjualanController::class, 'index']);
+// Route::post('transaksi', PenjualanController::class)->name('transaksi');
+// Route::get('transaksi', function () {
+//     $transaksi = PenjualanModel::all();
+
+//     return response()->json([
+//         'success' => true,
+//         'data' => $transaksi,
+//     ]);
+// });
+
+// //Route::get('transaksi/{transaksi}', [PenjualanController::class, 'show']);
+// Route::put('transaksi/{transaksi}', [PenjualanController::class, 'update']);
+// Route::delete('transaksi/{transaksi}', [PenjualanController::class, 'destroy']);
+
 Route::post('/register1', App\Http\Controllers\Api\RegisterController::class)->name('register1');
 // Route::post('/register', App\Http\Controllers\Api\RegisterController::class)->name('register');
 Route::post('/login', App\Http\Controllers\Api\LoginController::class)->name('login');
@@ -55,3 +74,7 @@ Route::post('barangs', [BarangController::class, 'store']);
 Route::get('barangs/{barang}', [BarangController::class, 'show']);
 Route::put('barangs/{barang}', [BarangController::class, 'update']);
 Route::delete('barangs/{barang}', [BarangController::class, 'destroy']);
+
+// m_barang
+Route::post('/barang1',[BarangController::class, 'store'])->name('barang1');
+Route::get('/barang1/{barang}', [BarangController::class, 'show']);
